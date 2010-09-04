cc2ba9b43990a75cb039f6aa26f3a201071c3c4e
Thomas 'Ted' Davis <trak3r@gmail.com>
2010-09-04 09:51:12 -0400

    fresh rails3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitter2rss::Application.config.secret_token = '43719b7ccad1425b93162c4e45020623537095a06f3a45fdf1a247478c80ca7d149323c0d7974baea181d816b9f15a84521658fd505ae1ad20b93beec85fb1d1'
