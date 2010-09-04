a989fb50fe4f8cc51c8aa2bbb2e821e3ddb3583d
Thomas 'Ted' Davis <trak3r@gmail.com>
2010-09-04 15:54:59 -0400

    got one successful pull. need to renew token every request?

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -2,7 +2,7 @@ class TimelineController < ApplicationController
   include OauthSystem
 
   def index
-    current_user = Member.find_by_token(params[:token])
+    self.current_user = Member.find_by_token(params[:token])
     @tweets = mentions
     raise @tweets.inspect
   end
